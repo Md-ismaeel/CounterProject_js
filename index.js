cf5becec3eb4f6d4a80175ffff9db72a79c0923b
Md Ismail <mdismaeelkhan345@gmail.com>
2024-01-12 19:45:58 +0530

    audio Sound Added

--- index.js
+++ index.js
@@ -2,21 +2,22 @@ let label = document.querySelector("#label #conter");
 let btn1 = document.querySelector(".btnIncrese");
 let btn2 = document.querySelector(".btnDecrease");
 let btn3 = document.querySelector(".btnReset");
+let audio = new Audio("analog-appliance-button-2-185277.mp3");
 
 let count = 0;
 
 btn1.addEventListener("click", (e) => {
+  audio.play();
   count += 1;
   label.innerText = count;
   console.log(count);
 });
 
 btn2.addEventListener("click", (e) => {
+  audio.play();
   if (count <= 0) {
     count = 0;
-    // btn2.disabled = true;
   } else {
-    // btn2.disabled = false;
     count -= 1;
   }
   label.innerText = count;
@@ -24,6 +25,7 @@ btn2.addEventListener("click", (e) => {
 });
 
 btn3.addEventListener("click", (e) => {
+  audio.play();
   count = 0;
   label.innerText = count;
   console.log(count);
